a40eaed142490e5c26b416f32c789f4be4d4da17
VSenna <contacriadapra.baixarapp@gmail.com>
2024-03-20 11:17:50 -0300

    testblink

--- main/main.c
+++ main/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
 
 void app_main(void)
 {
+    gpio_set_direction(2 output);
+
+
+    while(1){
+        gpio_set_level(2, 1);
+        vTaskDelay(vTaskDelay/portTICK_PERIOD_MS);
+        gpio_set_level(2, 0);
+        vTaskDelay(vTaskDelay/portTICK_PERIOD_MS);        
+    }
+
 
 }
